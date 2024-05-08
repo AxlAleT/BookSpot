af29fd88477668880d0cca362c0dbdd2c64aa219
Acvedo <168782503+Acvedo@users.noreply.github.com>
2024-05-08 15:54:16 -0600

    Update models.py

--- Ventas/models.py
+++ Ventas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    ID = models.DecimalField(primary_key=True, max_digits=10, decimal_places=0)
+    Cantidad = models.DecimalField(max_digits=5, decimal_places=0)
+    Precio = models.DecimalField(max_digits=5, decimal_places=2)
+    Titulo = models.CharField(max_length=100)
+    Autor  = models.CharField(max_length=100)
+    Editorial = models.CharField(max_length=100)
+
+    class Lib:
+        db_table = "Libro"
