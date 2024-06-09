9754891f3dd52cef3e7fcca094c678d7b926dcff
Axel Torres <axlalet@icloud.com>
2024-06-08 18:16:35 -0600

    El completar una venta actualiza la BD

--- bookspot/back/app/api/ventas/router.py
+++ bookspot/back/app/api/ventas/router.py
@@ -39,6 +39,44 @@ def get_libro():
         return jsonify(LibroResponseSchema().dump(libro_response))
     except (InvalidRequestException, BookNotFoundException, NotEnoughStockException) as e:
         return e.response
+    
+@ventas_bp.route('/completar/', methods=['PATCH'])
+def completar():
+    request_data = request.json
+    
+    if request_data is None:
+        raise InvalidRequestException()
+
+    try:
+        # Validar la solicitud
+        validate_request(request_data)
+        
+        # Obtener los elementos del pedido
+        items = request_data.get('items')
+
+        for item in items:
+            libro_id = item.get('id_libro')
+            cantidad = item.get('cantidad')
+
+            # Verificar si el libro existe y hay suficiente stock
+            libro = Libro.query.get(libro_id)
+            if libro is None:
+                raise BookNotFoundException(libro_id)
+            if libro.available_quantity < cantidad:
+                raise NotEnoughStockException(libro_id, cantidad, libro.available_quantity)
+            
+            # Actualizar la cantidad disponible del libro
+            libro.available_quantity -= cantidad
+        
+        # Confirmar los cambios en la base de datos
+        db.session.commit()
+        
+        return jsonify({"message": "Venta completada exitosamente."}), 200
+        
+    except (InvalidRequestException, BookNotFoundException, NotEnoughStockException) as e:
+        db.session.rollback()  # Deshacer cualquier cambio en caso de error
+        return e.response, 400
+
 
 def validate_request(request_data):
     try:
