041fd86d5616af4c9d27bf33b59a3e515f6a1737
Axel Torres <axlalet@icloud.com>
2024-07-01 20:23:20 -0600

    advertencia main

--- bookspot/back2/main.py
+++ bookspot/back2/main.py
@@ -1,6 +1,11 @@
 from app import create_app
 from app.base import init_db
 
+"""
+    Este archivo limpia la base de datos y la inicializa con valores por defecto.
+    Usar con precaucion, ya que se perderan todos los datos existentes en la base de datos.
+    en su lugar usar el comando 'flask run' en la terminal para iniciar la aplicacion.
+"""
 app = create_app()
 
 with app.app_context():
